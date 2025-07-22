9949432859bf065fb8c32a9c730e29ed28cd1d6b
Oluwaseun2006 <aromiremosturah@gmail.com>
2025-07-22 18:57:14 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: " malvin~1qkVVBRR#fCeU6phmyxq_0L_470ZFce84z9E0wKiOk-eiB6P2Rzk",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2347017495704", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
